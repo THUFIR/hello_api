f734c5f4970a570e5cd6c527a3352f75a296f973
thufir <thufir@mordor.bounceme.net>
2016-02-05 02:40:00 -0800

    increment

---
+++ src/main/java/net/bounceme/mordor/hello/HelloLibrary.java
@@ -0,0 +1,20 @@
+package net.bounceme.mordor.hello;
+
+public class HelloLibrary {
+
+  public static String hello(String... args) {
+	return "hello " + args;
+  }
+
+/*
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+*/
+  /*
+   @Test public void testHello() {
+   String message = "Hello World!";
+   Assert.assertEquals(12, message.length());
+   }
+   */
+}
