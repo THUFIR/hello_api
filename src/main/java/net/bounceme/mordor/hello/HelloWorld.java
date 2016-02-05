9385a2eee72249f8351363509f896be5c8387f17
thufir <thufir@mordor.bounceme.net>
2016-02-05 02:13:52 -0800

    increment

--- src/main/java/net/bounceme/mordor/hello/HelloWorld.java
+++ src/main/java/net/bounceme/mordor/hello/HelloWorld.java
@@ -1,16 +1,20 @@
 package net.bounceme.mordor.hello;
 
 public class HelloWorld {
+
+  public static String hello(String... args) {
+	return "hello " + args;
+  }
+
+/*
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
-  
+*/
   /*
    @Test public void testHello() {
    String message = "Hello World!";
    Assert.assertEquals(12, message.length());
    }
-   
    */
-  
 }
